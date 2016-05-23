0ba70dfb4d752d0439ba547f7905ae8a1cbd5f2a
Matheus Lima <matheusml90@gmail.com>
2016-05-23 17:16:20 -0300

    Adding stateless components

--- app/components/github/user-info.js
+++ app/components/github/user-info.js
@@ -1,28 +1,26 @@
 var React = require('react');
 var UserRepos = require('./user-repos');
 
-var UserInfo = React.createClass({
-  render: function () {
-    if (this.props.user) {
-      return (
-        <div className="row">
-          <div className="col-lg-4">
-            <img className="img-circle" src={this.props.user.avatar_url} alt="avatar" width="140" height="140" />
-            <h2>{this.props.user.login}</h2>
-            <p>{this.props.user.name}</p>
-            <p>Followers: {this.props.user.followers} / Following: {this.props.user.following}</p>
-            <p><a className="btn btn-default" href={this.props.user.html_url} role="button">View details</a></p>
-          </div>
-          <div className="col-lg-8">
-            <UserRepos />
-          </div>
+function UserInfo(props) {
+  if (props.user) {
+    return (
+      <div className="row">
+        <div className="col-lg-4">
+          <img className="img-circle" src={props.user.avatar_url} alt="avatar" width="140" height="140" />
+          <h2>{props.user.login}</h2>
+          <p>{props.user.name}</p>
+          <p>Followers: {props.user.followers} / Following: {props.user.following}</p>
+          <p><a className="btn btn-default" href={props.user.html_url} role="button">View details</a></p>
         </div>
-      );
-    }
-    else {
-      return (null);
-    }
+        <div className="col-lg-8">
+          <UserRepos />
+        </div>
+      </div>
+    );
   }
-});
+  else {
+    return (null);
+  }
+}
 
 module.exports = UserInfo;
--- app/components/github/user-repos.js
+++ app/components/github/user-repos.js
@@ -1,34 +1,25 @@
 var React = require('react');
 
-var UserRepos = React.createClass({
-  render: function() {
-    var repo = {
-      stargazers_count: '',
-      name: '',
-      description: '',
-      html_url: '',
-      open_issues: '',
-    };
-    return (
-      <div>
-        <h2>Repositories</h2>
-        <div ng-repeat="repo in repos">
-          <div className="thumbnail">
-            <div className="caption">
-              <h3>{repo.name}}
-                <span className="badge">{repo.stargazers_count}} STARS</span>
-              </h3>
-              <p>{repo.description}}</p>
-              <p>
-                <a href={repo.html_url} className="btn btn-primary" role="button">Repository</a>
-                <a href={repo.html_url + '/issues'} className="btn btn-default" role="button">Issues ({repo.open_issues}})</a>
-              </p>
-            </div>
+function UserRepos(props) {
+  return (
+    <div>
+      <h2>Repositories</h2>
+      <div ng-repeat="repo in repos">
+        <div className="thumbnail">
+          <div className="caption">
+            <h3>{props.repo.name}}
+              <span className="badge">{props.repo.stargazers_count}} STARS</span>
+            </h3>
+            <p>{props.repo.description}}</p>
+            <p>
+              <a href={props.repo.html_url} className="btn btn-primary" role="button">Repository</a>
+              <a href={props.repo.html_url + '/issues'} className="btn btn-default" role="button">Issues ({props.repo.open_issues}}) </a>
+            </p>
           </div>
         </div>
       </div>
-    );
-  }
-});
+    </div>
+  );
+}
 
 module.exports = UserRepos;
