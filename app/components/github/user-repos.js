086f4fc4ab91945726ab4dc7043f2da6eacbc73b
Matheus Lima <matheusml90@gmail.com>
2016-05-24 22:08:13 -0300

    Adding repos count

--- app/components/github/user-repos.js
+++ app/components/github/user-repos.js
@@ -1,30 +1,42 @@
 var React = require('react');
 
-function UserRepos(props) {
-  var repos = props.repos.map(function(repo, key) {
-    return (
-      <div key={key} className="thumbnail">
-        <div className="caption">
-          <h3>{repo.name}
-            <span className="badge">{repo.stargazers_count} STARS</span>
-          </h3>
-          <p>{repo.description}</p>
-          <p>
-            <a href={repo.html_url} className="btn btn-primary" role="button">Repository</a>
-            <a href={repo.html_url + '/issues'} className="btn btn-default" role="button">Issues ({repo.open_issues}) </a>
-          </p>
+var UserRepos = React.createClass({
+  getInitialState: function() {
+    return {
+      reposLength: 0,
+    }
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({ reposLength: nextProps.repos.length });
+  },
+
+  render: function () {
+    var repos = this.props.repos.map(function (repo, key) {
+      return (
+        <div key={key} className="thumbnail">
+          <div className="caption">
+            <h3>{repo.name}
+              <span className="badge">{repo.stargazers_count} STARS</span>
+            </h3>
+            <p>{repo.description}</p>
+            <p>
+              <a href={repo.html_url} className="btn btn-primary" role="button">Repository</a>
+              <a href={repo.html_url + '/issues'} className="btn btn-default" role="button">Issues ({repo.open_issues}) </a>
+            </p>
+          </div>
         </div>
+      );
+    });
+
+    return (
+      <div>
+        <h2>{this.state.reposLength} repositories</h2>
+        {repos}
       </div>
     );
-  });
-
-  return (
-    <div>
-      <h2>Repositories</h2>
-      {repos}
-    </div>
-  );
-}
+  }
+});
 
 UserRepos.propTypes = {
   repos: React.PropTypes.array,
